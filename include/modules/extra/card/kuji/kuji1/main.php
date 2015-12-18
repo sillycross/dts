31f941cd19e46e48164dbb4079b881ffbb48b034
sillycross <haoranxu510@gmail.com>
2015-12-18 03:18:19 -0500

    无聊的抽卡游戏

--- include/modules/extra/card/kuji/kuji1/main.php
+++ include/modules/extra/card/kuji/kuji1/main.php
@@ -4,10 +4,10 @@ namespace kuji1
 {
 	function init() {}
 	
-	function kujidraw1(&$pa){
+	function kujidraw1(&$pa, $is_dryrun = false){
 		if (eval(__MAGIC__)) return $___RET_VALUE;
 		eval(import_module('cardbase','kuji1'));
-		if ($pa['gold']<1000) return -1;
+		if ($pa['gold']<1000 && !$is_dryrun) return -1;
 		$rr=array();
 		$r=rand(1,$cw);
 		if ($r<=$sw){
@@ -19,9 +19,12 @@ namespace kuji1
 		}
 		$c=count($arr)-1;
 		$r=$arr[rand(0,$c)];
-		if ((\cardbase\get_card($r,$pa))!=1){
-			\cardbase\get_qiegao(30,$pa);
-			$pa['gold']+=30;
+		if (!$is_dryrun)
+		{
+			if ((\cardbase\get_card($r,$pa))!=1){
+				\cardbase\get_qiegao(30,$pa);
+				$pa['gold']+=30;
+			}
 		}
 		$rr[0]=$r;
 		for ($i=1;$i<=10;$i++){
@@ -37,12 +40,15 @@ namespace kuji1
 			}
 			$c=count($arr)-1;
 			$r=$arr[rand(0,$c)];
-			if ((\cardbase\get_card($r,$pa))==1){
-				\cardbase\get_qiegao(-100,$pa);
-				$pa['gold']-=100;
-			}else{
-				\cardbase\get_qiegao(-70,$pa);
-				$pa['gold']-=70;
+			if (!$is_dryrun)
+			{
+				if ((\cardbase\get_card($r,$pa))==1){
+					\cardbase\get_qiegao(-100,$pa);
+					$pa['gold']-=100;
+				}else{
+					\cardbase\get_qiegao(-70,$pa);
+					$pa['gold']-=70;
+				}
 			}
 			$rr[$i]=$r;
 		}		
