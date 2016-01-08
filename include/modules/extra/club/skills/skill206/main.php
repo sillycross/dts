4629940b929507894308da29dbfbd23a8a43d685
sillycross <haoranxu510@gmail.com>
2016-01-08 12:17:08 +0800

    added club21

--- include/modules/extra/club/skills/skill206/main.php
+++ include/modules/extra/club/skills/skill206/main.php
@@ -45,8 +45,7 @@ namespace skill206
 		if (!\skillbase\skill_query(206,$pa) || !check_unlocked206($pa)) return $chprocess($pa,$pd,$active);
 		$chprocess($pa,$pd,$active);
 		if ((($pd['hp']*0.85)<(0.15*$pa['dmg_dealt']))&&($pd['hp']>0)&&($pa['wep_kind']=='G')){
-			$pa['dmg_dealt']+=$pd['hp'];
-			$pd['hp']=0;
+			$pa['dmg_dealt']=$pd['hp'];
 			eval(import_module('logger'));
 			if ($active) $log .= "<span class=\"red\">你的攻击直接将敌人爆头！</span><br>";
 			else $log .= "<span class=\"red\">敌人的攻击直接将你爆头！</span><br>";
