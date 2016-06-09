f125ed1dd181ca947dd007db1dc8a44fcd471ac3
dtsLein <2665884573@qq.com>
2016-06-09 20:47:09 +0800

    正义锉刀
    明斯克航天局已经决定了

--- include/modules/extra/club/skills/skill253/main.php
+++ include/modules/extra/club/skills/skill253/main.php
@@ -2,7 +2,7 @@
 
 namespace skill253
 {
-	$ragecost=10;
+	$ragecost=25;
 	
 	function init() 
 	{
@@ -74,7 +74,7 @@ namespace skill253
 	{
 		if (eval(__MAGIC__)) return $___RET_VALUE;
 		if ($pa['bskill']!=253) return $chprocess($pa,$pd,$active);
-		$r = min(300,round($pa['lvl']*($pa['rage']+get_rage_cost253())/5));
+		$r = min(300,round($pa['lvl']*($pa['rage']+get_rage_cost253())/6));
 		eval(import_module('logger'));
 		if ($active)
 			$log.='<span class="yellow">「天威」使你的熟练度暂时增加了'.$r.'点！</span><br>';
@@ -102,7 +102,7 @@ namespace skill253
 	{
 		if (eval(__MAGIC__)) return $___RET_VALUE;
 		$val=$chprocess($pa, $pd, $active);
-		if ($pa['bskill']==253) $val*=1.3;
+		if ($pa['bskill']==253) $val*=1.1;
 		return $val;
 	}
 		
