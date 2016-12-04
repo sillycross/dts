239b966ddf6812f8e2d58d1d983f2b210407401b
winddramon <ranshiin@qq.com>
2016-12-04 19:02:45 +0800

    update

--- include/modules/extra/instance/gtype17_tutorial/tutorial/module.inc.php
+++ include/modules/extra/instance/gtype17_tutorial/tutorial/module.inc.php
@@ -3,11 +3,11 @@
 namespace tutorial
 {
 	////////// MODULE HEADER START ///////////////
-	$___MODULE_dependency = 'sys player map logger itemmain explore itemshop skill1000';
+	$___MODULE_dependency = 'sys player map logger itemmain metman itemshop explore skill1000';
 	$___MODULE_dependency_optional = '';
 	$___MODULE_conflict = '';
-	$___MODULE_codelist = 'main.php ../config/tutorial.config.php';
-	$___MODULE_templatelist = 'tutorial tutorial_itemfind';
+	$___MODULE_codelist = 'main.php tutorial.config.php';
+	$___MODULE_templatelist = 'tutorial ../config/tutorial_itemfind';
 	////////// MODULE HEADER END /////////////////
 	require __INIT_MODULE__(__NAMESPACE__,__DIR__);
 }
