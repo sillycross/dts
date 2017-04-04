92019e94d17de4313ddf2691bbf3891363d67838
winddramon <ranshiin@qq.com>
2017-04-05 02:04:08 +0800

    修正了房间方面的一个BUG

--- index.php
+++ index.php
@@ -19,8 +19,9 @@ $adminmsg = file_get_contents('./gamedata/adminmsg.htm') ;
 $systemmsg = file_get_contents('./gamedata/systemmsg.htm') ;
 
 $roomlist = Array();
-$result = $db->query("SELECT * FROM {$gtablepre}rooms WHERE status > 0");
-while ($data = $db->fetch_array($result))
+
+$roomresult = $db->query("SELECT * FROM {$gtablepre}rooms WHERE status > 0");
+while ($data = $db->fetch_array($roomresult))
 {
 	if ($data['status']==1)
 	{
@@ -73,7 +74,7 @@ while ($data = $db->fetch_array($result))
 			$roomlist[$data['roomid']]['roomdata'] = $roomdata;
 		}
 	}
-	else  if ($data['status']==2)
+	elseif ($data['status']==2)
 	{
 		if (file_exists(GAME_ROOT.'./gamedata/tmp/rooms/'.$data['roomid'].'.txt'))
 		{
