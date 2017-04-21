9576500d7bcb7a5c687d2946be38e8546580fc97
winddramon <ranshiin@qq.com>
2017-04-21 19:36:30 +0800

    加了一个自动生成差分包的bat，用git shell调用打开即可

---
+++ auto_diff.bat
@@ -0,0 +1,19 @@
+@echo off
+setlocal EnableDelayedExpansion
+set /p sha1=请输入第一个历史版本的commit_id，忽略为使用head
+set /p sha2=请输入第二个历史版本的commit_id，忽略为使用head
+if not defined sha1 set sha1=head
+if not defined sha2 set sha2=head
+git diff %sha1% %sha2% --name-only > auto_diff.tmp
+for /f "delims=" %%i IN (auto_diff.tmp) DO (
+	call :parse_filename ..\auto_diff\.\%%i
+  if exist %%~fsi (
+    xcopy /s /y /i %%~fsi !return!
+  )
+)
+del /q auto_diff.tmp
+exit
+
+:parse_filename
+set return=%~dp1
+goto :eof
