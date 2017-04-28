e66bc450126dda4e262f15f5ab2e1b1bb8a04cc7
winddramon <ranshiin@qq.com>
2017-04-29 02:06:16 +0800

    一些细节数据修改

--- include/modules/extra/searchmemory/config/searchmemory.config.php
+++ include/modules/extra/searchmemory/config/searchmemory.config.php
@@ -3,7 +3,7 @@
 namespace searchmemory
 {
 	$searchmemory_max_slotnum = 5;//最大记忆数
-	$searchmemory_battle_active_debuff = 0.7;//迎战记忆中的敌人时，玩家先制率的减成
+	$searchmemory_battle_active_debuff = 0.6;//迎战记忆中的敌人时，玩家先制率乘以的百分比
 }
 
 ?>
