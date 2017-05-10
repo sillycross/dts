77baa0c7cd3d5138dff61313b642b25cb0f2fb27
winddramon <ranshiin@qq.com>
2017-05-11 02:20:46 +0800

    漏了一个设定

--- include/modules/extra/attr/ex_hp_def/module.inc.php
+++ include/modules/extra/attr/ex_hp_def/module.inc.php
@@ -4,7 +4,7 @@ namespace ex_hp_def
 {
 	////////// MODULE HEADER START ///////////////
 	$___MODULE_dependency = 'logger attack attrbase itemmain';
-	$___MODULE_dependency_optional = '';
+	$___MODULE_dependency_optional = 'skill479';
 	$___MODULE_conflict = '';
 	$___MODULE_codelist = 'main.php';
 	$___MODULE_templatelist = '';
