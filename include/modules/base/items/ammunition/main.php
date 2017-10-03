d7ccc286489d7f94363ed950b28c66eda411f496
winddramon <ranshiin@qq.com>
2017-10-03 22:29:26 +0800

    弹药提示

--- include/modules/base/items/ammunition/main.php
+++ include/modules/base/items/ammunition/main.php
@@ -2,6 +2,16 @@
 
 namespace ammunition
 {
+	$ammukind = array(
+		'WJ' => array('GBh', 1),
+		'e' => array('GBe', 10),
+		'w' => array('GBe', 10),
+		'i' => array('GBi', 10),
+		'u' => array('GBi', 10),
+		'r' => array('GBr', 20),
+		'WG' => array('GB', 6),
+	);
+	
 	function init()
 	{
 		eval(import_module('itemmain'));
@@ -13,6 +23,19 @@ namespace ammunition
 		
 		$itemspkinfo['o'] = '一发';
 		
+	}
+	
+	function check_ammukind($cwepk, $cwepsk){
+		if (eval(__MAGIC__)) return $___RET_VALUE;
+		eval(import_module('ammunition'));
+		$retk = 'GB'; $retn = 6;
+		foreach($ammukind as $ak => $av){
+			if((strpos($ak, 'W')===0 && strpos($cwepk, $ak) === 0) || (strpos($ak, 'W')!==0 && strpos($cwepsk, $ak) !== false)){
+				$retk = $av[0]; $retn = $av[1];
+				break;
+			}
+		}
+		return array($retk, $retn);
 	}
 	
 	function itemuse_ugb(&$theitem)
@@ -35,50 +58,57 @@ namespace ammunition
 			$mode = 'command';
 			return;
 		}
-		if (strpos ($wepk,'WG')===false){
-			if ($itmk=='GBh'){
-				$bulletnum = 1;	
-			}else{
-				$log .= "<span class=\"red\">枪械类型和弹药类型不匹配。</span><br>";
-				$mode = 'command';
-				return;
-			}
+		
+		list($bulletkind, $bulletnum) = check_ammukind($wepk, $wepsk);
+		if($itmk != $bulletkind){
+			$log .= "<span class='red'>弹药类型不匹配，需要</span><span class='yellow'>$iteminfo[$bulletkind]</span>。<br>";
+			$mode = 'command';
+			return;
 		}
-		elseif (strpos ( $wepsk, 'e' ) !== false || strpos ( $wepsk, 'w' ) !== false) {
-			if ($itmk == 'GBe') {
-				$bulletnum = 10;
-			} else {
-				$log .= "<span class=\"red\">枪械类型和弹药类型不匹配。</span><br>";
-				$mode = 'command';
-				return;
-			}
-		} elseif (strpos ( $wepsk, 'i' ) !== false || strpos ( $wepsk, 'u' ) !== false) {
-			if ($itmk == 'GBi') {
-				$bulletnum = 10;
-			} else {
-				$log .= "<span class=\"red\">枪械类型和弹药类型不匹配。</span><br>";
-				$mode = 'command';
-				return;
-			}
-		} else {
-			if (strpos ( $wepsk, 'r' ) !== false) {
-				if ($itmk == 'GBr') {
-					$bulletnum = 20;
-				} else {
-					$log .= "<span class=\"red\">枪械类型和弹药类型不匹配。</span><br>";
-					$mode = 'command';
-					return;
-				}
-			} else {
-				if ($itmk == 'GB') {
-					$bulletnum = 6;
-				} else {
-					$log .= "<span class=\"red\">枪械类型和弹药类型不匹配。</span><br>";
-					$mode = 'command';
-					return;
-				}
-			}
-		}
+//		if (strpos ($wepk,'WG')===false){
+//			if ($itmk=='GBh'){
+//				$bulletnum = 1;	
+//			}else{
+//				$log .= "<span class=\"red\">枪械类型和弹药类型不匹配。</span><br>";
+//				$mode = 'command';
+//				return;
+//			}
+//		}
+//		elseif (strpos ( $wepsk, 'e' ) !== false || strpos ( $wepsk, 'w' ) !== false) {
+//			if ($itmk == 'GBe') {
+//				$bulletnum = 10;
+//			} else {
+//				$log .= "<span class=\"red\">枪械类型和弹药类型不匹配。</span><br>";
+//				$mode = 'command';
+//				return;
+//			}
+//		} elseif (strpos ( $wepsk, 'i' ) !== false || strpos ( $wepsk, 'u' ) !== false) {
+//			if ($itmk == 'GBi') {
+//				$bulletnum = 10;
+//			} else {
+//				$log .= "<span class=\"red\">枪械类型和弹药类型不匹配。</span><br>";
+//				$mode = 'command';
+//				return;
+//			}
+//		} else {
+//			if (strpos ( $wepsk, 'r' ) !== false) {
+//				if ($itmk == 'GBr') {
+//					$bulletnum = 20;
+//				} else {
+//					$log .= "<span class=\"red\">枪械类型和弹药类型不匹配。</span><br>";
+//					$mode = 'command';
+//					return;
+//				}
+//			} else {
+//				if ($itmk == 'GB') {
+//					$bulletnum = 6;
+//				} else {
+//					$log .= "<span class=\"red\">枪械类型和弹药类型不匹配。</span><br>";
+//					$mode = 'command';
+//					return;
+//				}
+//			}
+//		}
 		if ($weps == $nosta) {
 			$weps = 0;
 		}
