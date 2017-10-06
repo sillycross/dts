e22623d41c718f378eab5e3a60fd72aa9d9790de
winddramon <ranshiin@qq.com>
2017-10-06 23:40:51 +0800

    version

--- include/modules/core/sys/config/version.config.php
+++ include/modules/core/sys/config/version.config.php
@@ -1,6 +1,6 @@
 <?php
 //游戏版本
-$gameversion = 'N.E.W. v1.42';
+$gameversion = 'N.E.W. v1.5';
 //版权信息
 $gamecredits = <<<GAMECREDITS
 	<a href="http://www.amarilloviridian.com">DTS $gameversion coded by sillycross, Lein, Yoshiko_G AND illustrated by KHIBIKI. 感谢所有为这个游戏做出贡献的人</a><br>
