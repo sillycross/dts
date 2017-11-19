b4c7f99da37b3a7c53d85dd0b07e62b147ee1d17
winddramon <ranshiin@qq.com>
2017-11-19 23:03:33 +0800

    1.7.0 EARLY FIX
    hmmm减少性能消耗

--- winner.php
+++ winner.php
@@ -1,10 +1,9 @@
 <?php
 
 define('CURSCRIPT', 'winner');
+define('IN_GAME', true);
 defined('GAME_ROOT') || define('GAME_ROOT', dirname(__FILE__).'/');
-define('NO_MOD_LOAD', TRUE);	//不在common.inc载入任何模块
-define('NO_SYS_UPDATE', TRUE);
-require GAME_ROOT.'./include/common.inc.php';
+require GAME_ROOT.'./include/global.func.php';
 		
 $url = 'http://'.$_SERVER['HTTP_HOST'].substr($_SERVER['PHP_SELF'],0,-10).'command.php';
 $context = array('page'=>'command_winner');
