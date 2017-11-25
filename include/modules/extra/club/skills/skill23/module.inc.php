624beca962486847618c64953d22113fc2c89d8d
winddramon <ranshiin@qq.com>
2017-11-26 00:22:57 +0800

    1.7.4B
    宝骑技能显示

--- include/modules/extra/club/skills/skill23/module.inc.php
+++ include/modules/extra/club/skills/skill23/module.inc.php
@@ -6,7 +6,7 @@ namespace skill23
 	$___MODULE_dependency = 'skillbase clubbase sys player logger itemmain input lvlctl';
 	$___MODULE_dependency_optional = '';
 	$___MODULE_conflict = '';
-	$___MODULE_codelist = 'main.php';
+	$___MODULE_codelist = 'main.php config/skill23.config.php';
 	$___MODULE_templatelist = 'desc profilecmd gemming';
 	////////// MODULE HEADER END /////////////////
 	require __INIT_MODULE__(__NAMESPACE__,__DIR__);
