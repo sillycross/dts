eccd0c4e1a5acfbbcfc487a2734834aa620fe610
winddramon <ranshiin@qq.com>
2017-12-07 22:30:27 +0800

    1.7.14 early 2
    丢弃道具按钮化

--- include/modules/extra/searchmemory/config/searchmemory.config.php
+++ include/modules/extra/searchmemory/config/searchmemory.config.php
@@ -2,8 +2,15 @@
 
 namespace searchmemory
 {
+	$searchmemory_disabled_gtype = array(1);//不启用探索记忆的游戏模式
 	$searchmemory_max_slotnum = 5;//最大记忆数
 	$searchmemory_battle_active_debuff = -40;//迎战记忆中的敌人时，玩家先制率的减成值
+	
+	$weather_memory_loss = array(//各天气对记忆（视野）的影响
+		8 => -2,//起雾
+		9 => -4,//浓雾
+		12 => -4//暴风雪
+	);
 }
 
 ?>
