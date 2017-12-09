1309d6740a1040699f08c32b417dd04c9c1d6457
winddramon <ranshiin@qq.com>
2017-12-09 12:02:24 +0800

    1.7.15 test
    试试看

--- roomcmd.php
+++ roomcmd.php
@@ -546,6 +546,7 @@ if(room_get_vars($roomdata,'soleroom')){//永续房只进行离开判定
 			$roomdata['readystat']=2;
 			$roomdata['timestamp']++;
 			room_save_broadcast($room_id_r,$roomdata);
+			usleep(100000);//性能歧视
 			$db->query("UPDATE {$gtablepre}game SET groomstatus=40 WHERE groomid='$room_id_r'");
 			$roomdata['readystat']=0;
 			$roomdata['chatdata']=room_init($roomdata['roomtype'])['chatdata'];
