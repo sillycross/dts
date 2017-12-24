015582f63e3af7862a64495a808fd8bfdab6d8ae
winddramon <ranshiin@qq.com>
2017-12-25 00:11:51 +0800

    1.8.9 fix

--- include/modules/extra/card/cardbase/module.inc.php
+++ include/modules/extra/card/cardbase/module.inc.php
@@ -3,7 +3,7 @@
 namespace cardbase
 {
 	////////// MODULE HEADER START ///////////////
-	$___MODULE_dependency = 'sys player attack itemmix bubblebox';
+	$___MODULE_dependency = 'sys player attack itemmix bubblebox metman';
 	$___MODULE_dependency_optional = '';
 	$___MODULE_conflict = '';
 	$___MODULE_codelist = 'main.php config/card.config.php';
