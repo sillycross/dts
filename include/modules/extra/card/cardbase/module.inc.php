0288b9eb229cbe318e75889aaea245ec223bf930
winddramon <ranshiin@qq.com>
2017-12-26 23:25:58 +0800

    1.9.0 early 4
    给我小马一张卡图

--- include/modules/extra/card/cardbase/module.inc.php
+++ include/modules/extra/card/cardbase/module.inc.php
@@ -7,7 +7,7 @@ namespace cardbase
 	$___MODULE_dependency_optional = '';
 	$___MODULE_conflict = '';
 	$___MODULE_codelist = 'main.php config/card.config.php';
-	$___MODULE_templatelist = 'cardflip_back cardflip_result';
+	$___MODULE_templatelist = 'cardflip_back cardflip_result card_frame';
 	////////// MODULE HEADER END /////////////////
 	require __INIT_MODULE__(__NAMESPACE__,__DIR__);
 }
