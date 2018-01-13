a54f7bb52656446a0cda922a67a9c2d2128b4a26
winddramon <ranshiin@qq.com>
2018-01-14 01:37:17 +0800

    1.8.18
    改写了同调和超量

--- include/modules/base/itemmix/itemmix_sync/main.php
+++ include/modules/base/itemmix/itemmix_sync/main.php
@@ -4,10 +4,13 @@ namespace itemmix_sync
 {
 	function init() 
 	{
-		eval(import_module('itemmain'));
+		eval(import_module('itemmain','itemmix'));
 		$itemspkinfo['s'] = '调整';
 		$itemspkdesc['s']='参与同调合成的必要属性';
-		$itemspkremark['s']='……';
+		$itemspkinfo['^001^'] = '同调';
+		$itemspkdesc['^001^']='这一道具是同调合成出来的';
+		$itemspkremark['s']=$itemspkremark['^001^']='……';
+		$mix_type['sync'] = '同调';
 	}
 	
 	function get_itemmix_sync_filename(){
@@ -55,8 +58,9 @@ namespace itemmix_sync
 		eval(import_module('sys','player','itemmix'));
 		//判断是否存在调整，并计算总星数
 		$star = 0;
-		$tunner = array();
+		$tunner = $stuff = array();
 		foreach($mlist as $mval){
+			$stuff[] = ${'itm'.$mval};
 			list($mstar, $mtunner) = itemmix_star_culc_sync($mval);
 			if($mstar == 0){
 				$star = 0;break;
@@ -81,78 +85,109 @@ namespace itemmix_sync
 					foreach($mlist as $mi){
 						$mname[] = \itemmix\itemmix_name_proc(${'itm'.$mi});
 					}
-					sort($mname);sort($req);
-					if($mname != $req) $preqflag = false;
+					//如果素材没有满足则认为无法合成
+					foreach($req as $rv){
+						if('st'==$rv){//调整要求是同调
+							$tunnersk = ${'itmsk'.$tunner[0]};
+							if(strpos($tunnersk, '^001^')===false) $preqflag = false;
+						}elseif(strpos($rv,'sm')===0){//调整以外要求是同调
+							$smnum = (int)substr($rv,2);
+							foreach($mlist as $mi){
+								if(!in_array($mi, $tunner)){
+									$misk = ${'itmsk'.$mi};
+									if(strpos($misk, '^001^')===false) {
+										$preqflag = false;
+										break;
+									}
+								}
+							}
+							if(count($mlist) <= $smnum) $preqflag = false;//素材数目不足
+						}else{//其他，认为是名字要求
+							if(!in_array($rv, $mname)) $preqflag = false;
+						}
+					}
 				}
 				if($pstar == $star && $preqflag){
-					if(!isset($chc_res[$star])){//用键名记录星数和素材数方便提示
-						$chc_res[$star] = array('list' => $mlist, 'choices' => array($pra));
-					}else{
-						$chc_res[$star]['choices'][] = $pra;
-					}
+					if(empty($chc_res[$star])) $chc_res[$star] = array();
+					//用键名记录星数和素材数方便提示
+					$chc_res[$star][] = array('stuff' => $stuff, 'list' => $mlist, 'result' => $pra, 'type' => 'sync');
 				}
 			}
 		}
 		return $chc_res;
 	}
 	
-	function itemmix($mlist, $itemselect=-1) 
-	{
+	function itemmix_get_result($mlist){
 		if (eval(__MAGIC__)) return $___RET_VALUE;
-		eval(import_module('sys','player','logger','itemmix'));
+		$ret = $chprocess($mlist);
 		$chc_res = itemmix_sync_check($mlist);
-
-		//无满足条件的同调结果，失败
-		if (!$chc_res) return $chprocess($mlist, $itemselect);	
-		$chc = array_pop($chc_res);
-		if ($itemselect==-1)
-		{
-			$mask=0;
-			foreach($chc['list'] as $k)
-				if (1<=$k && $k<=6)
-					$mask|=(1<<((int)$k-1));
-				
-			$cmd.='<input type="hidden" id="mode" name="mode" value="itemmain">';
-			$cmd.='<input type="hidden" id="command" name="command" value="itemmix">';
-			$cmd.='<input type="hidden" id="mixmask" name="mixmask" value="'.$mask.'">';
-			$cmd.='<input type="hidden" id="itemselect" name="itemselect" value="999">';
-			$cmd.= "请选择同调结果<br><br>";
-			$sync = count($chc['choices']);
-			for($i=0;$i<$sync;$i++){
-				$tn=$chc['choices'][$i][0];
-				$cmd.="<input type=\"button\" class=\"cmdbutton\"  style=\"width:200\" value=\"".$tn."\" onclick=\"$('itemselect').value='".$i."';postCmd('gamecmd','command.php');this.disabled=true;\">";
+		if($chc_res){
+			foreach($chc_res as $cv) {
+				foreach($cv as $v){
+					$ret[] = $v;
+				}
 			}
-			$cmd.="<input type=\"button\" class=\"cmdbutton\"  style=\"width:200\" value=\"返回\" onclick=\"postCmd('gamecmd','command.php');this.disabled=true;\">";
-			return;
 		}
-		else
-		{
-			$i=(int)$itemselect;
-			if ($i<0 || $i > count($chc['choices']) - 1)
-			{
-				$mode='command'; return; 
-			}
-			foreach($chc['list'] as $val)
-			{
-				\itemmix\itemreduce('itm'.$val);
-			}
-			list($itm0,$itmk0,$itme0,$itms0,$itmsk0) = $chc['choices'][$i];
-			addnews($now,'syncmix',$name,$itm0);
-			\itemmain\itemget();
-			$mode = 'command';
-			return;
-		}
-		$chprocess($mlist, $itemselect);
+		return $ret;
 	}
 	
-	function parse_news($nid, $news, $hour, $min, $sec, $a, $b, $c, $d, $e, $exarr = array())
-	{
-		if (eval(__MAGIC__)) return $___RET_VALUE;
-		eval(import_module('sys'));
-		if($news == 'syncmix') 
-			return "<li id=\"nid$nid\">{$hour}时{$min}分{$sec}秒，<span class=\"lime\">{$a}同调合成了{$b}</span></li>";
-		return $chprocess($nid, $news, $hour, $min, $sec, $a, $b, $c, $d, $e, $exarr);
-	}
+//	function itemmix($mlist, $itemselect=-1) 
+//	{
+//		if (eval(__MAGIC__)) return $___RET_VALUE;
+//		eval(import_module('sys','player','logger','itemmix'));
+//		$chc_res = itemmix_sync_check($mlist);
+//
+//		//无满足条件的同调结果，失败
+//		if (!$chc_res) return $chprocess($mlist, $itemselect);	
+//		$chc = array_pop($chc_res);
+//		if ($itemselect==-1)
+//		{
+//			$mask=0;
+//			foreach($chc['list'] as $k)
+//				if (1<=$k && $k<=6)
+//					$mask|=(1<<((int)$k-1));
+//				
+//			$cmd.='<input type="hidden" id="mode" name="mode" value="itemmain">';
+//			$cmd.='<input type="hidden" id="command" name="command" value="itemmix">';
+//			$cmd.='<input type="hidden" id="mixmask" name="mixmask" value="'.$mask.'">';
+//			$cmd.='<input type="hidden" id="itemselect" name="itemselect" value="999">';
+//			$cmd.= "请选择同调结果<br><br>";
+//			$sync = count($chc['result']);
+//			for($i=0;$i<$sync;$i++){
+//				$tn=$chc['result'][$i][0];
+//				$cmd.="<input type=\"button\" class=\"cmdbutton\"  style=\"width:200\" value=\"".$tn."\" onclick=\"$('itemselect').value='".$i."';postCmd('gamecmd','command.php');this.disabled=true;\">";
+//			}
+//			$cmd.="<input type=\"button\" class=\"cmdbutton\"  style=\"width:200\" value=\"返回\" onclick=\"postCmd('gamecmd','command.php');this.disabled=true;\">";
+//			return;
+//		}
+//		else
+//		{
+//			$i=(int)$itemselect;
+//			if ($i<0 || $i > count($chc['result']) - 1)
+//			{
+//				$mode='command'; return; 
+//			}
+//			foreach($chc['list'] as $val)
+//			{
+//				\itemmix\itemreduce('itm'.$val);
+//			}
+//			list($itm0,$itmk0,$itme0,$itms0,$itmsk0) = $chc['result'][$i];
+//			addnews($now,'syncmix',$name,$itm0);
+//			\itemmain\itemget();
+//			$mode = 'command';
+//			return;
+//		}
+//		$chprocess($mlist, $itemselect);
+//	}
+//	
+//	function parse_news($nid, $news, $hour, $min, $sec, $a, $b, $c, $d, $e, $exarr = array())
+//	{
+//		if (eval(__MAGIC__)) return $___RET_VALUE;
+//		eval(import_module('sys'));
+//		if($news == 'syncmix') 
+//			return "<li id=\"nid$nid\">{$hour}时{$min}分{$sec}秒，<span class=\"lime\">{$a}同调合成了{$b}</span></li>";
+//		return $chprocess($nid, $news, $hour, $min, $sec, $a, $b, $c, $d, $e, $exarr);
+//	}
 }
 
 ?>
