baf62cf7b0694684f2874d072ad2307bfe04533e
winddramon <ranshiin@qq.com>
2018-03-19 01:02:33 +0800

    1.10.3 fix 4

--- include/modules/core/sys/init.php
+++ include/modules/core/sys/init.php
@@ -63,7 +63,7 @@ namespace sys
 		//统一获取用户数据备用
 		global $cudata, $userdb_foreced_local;
 		if (isset(${$gtablepre.'user'})){
-			$userdb_foreced_local = 1;
+			if(CURSCRIPT == 'command') $userdb_foreced_local = 1;
 			//强制读取本地
 			$cudata = fetch_udata_by_username(${$gtablepre.'user'});
 			$userdb_foreced_local = 0;
