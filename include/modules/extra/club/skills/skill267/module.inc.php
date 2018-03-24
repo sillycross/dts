1f4e79df47dc9d56514fc7d06ebebe11f6b17036
winddramon <ranshiin@qq.com>
2018-03-24 14:36:45 +0800

    1.11.2 early 1

--- include/modules/extra/club/skills/skill267/module.inc.php
+++ include/modules/extra/club/skills/skill267/module.inc.php
@@ -3,7 +3,7 @@
 namespace skill267
 {
 	////////// MODULE HEADER START ///////////////
-	$___MODULE_dependency = 'clubbase skillbase player battle';
+	$___MODULE_dependency = 'clubbase skillbase player battle attack';
 	$___MODULE_dependency_optional = '';
 	$___MODULE_conflict = '';
 	$___MODULE_codelist = 'main.php';
