1b539cf4abd4a0839de7f908919d407b34935195
winddramon <ranshiin@qq.com>
2018-06-09 22:42:42 +0800

    1.12.0 early 2

--- include/modules/base/opening/main.php
+++ include/modules/base/opening/main.php
@@ -29,9 +29,10 @@ namespace opening
 	function show_opening()
 	{
 		if (eval(__MAGIC__)) return $___RET_VALUE;
-		eval(import_module('sys','player','opening'));
+		eval(import_module('sys','player','opening','logger'));
 		if(in_game_opening_available()){
 			if(\skillbase\skill_query(1003) && !\skillbase\skill_getvalue(1003,'opening_skip')) {
+				$log .= ' ';
 				$main = MOD_OPENING_STORYBOARD_CONTAINER;
 				ob_start();
 				include template(MOD_OPENING_CMD_SKIP_OP);
