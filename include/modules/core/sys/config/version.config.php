1a58f53510256ebb8d3cea01bcd4d4db376485f1
winddramon <ranshiin@qq.com>
2018-06-27 00:42:37 +0800

    1.12.0 fix 2.0

--- include/modules/core/sys/config/version.config.php
+++ include/modules/core/sys/config/version.config.php
@@ -8,6 +8,6 @@ $gamecredits = <<<GAMECREDITS
 	<a href="http://www.amarilloviridian.com">Reconsituted from 常磐大逃杀 GE600 by 冴月 麟, Yoshiko_G, sillycross, Lein.</a><br>
 	<a>生存游戏 - BRA简体版V3.0.2 translated by azazil@loongyou.com</a><br>
 	<a href="http://www.b-r-u.net/">BRU (Battle Royale Ultimate) V0.22</a><br>
-	感谢所有为这个游戏做出贡献的人
+	<a>感谢所有为这个游戏做出贡献的人</a>
 GAMECREDITS;
 ?>
