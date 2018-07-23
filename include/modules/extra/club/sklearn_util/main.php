24771ee988779508e7d0087d98543179968ea84e
winddramon <ranshiin@qq.com>
2018-07-23 23:32:30 +0800

    1.12.6.2

--- include/modules/extra/club/sklearn_util/main.php
+++ include/modules/extra/club/sklearn_util/main.php
@@ -21,73 +21,28 @@ namespace sklearn_util
 	{
 		if (eval(__MAGIC__)) return $___RET_VALUE;
 		eval(import_module('player','clubbase'));
-		echo '<table border="0" cellSpacing=0 cellPadding=0 style="table-layout:fixed;" width="653px">';
-		echo '<tr><td class="b1" width="80px">技能来源</td>';
 		
 		$is_show_cost=$callback_funcname('show_cost');
 		$caller_id=$callback_funcname('caller_id');
 		
-		for ($i=1; $i<=4; $i++) 
-		{
-			if ($is_show_cost)
-				echo '<td class="b1" width="40px">技能</td><td class="b1" width="20px">费</td><td class="b1" width="80px">操作</td>';
-			else  echo '<td class="b1" width="60px">技能</td><td class="b1" width="80px">操作</td>';
-		}
-		echo '</tr>';
-		
 		$___TEMP_tlis=Array();
+		$lis = Array();
 		foreach ($clublist as $nowclub => $arr)
 			if ($nowclub!=$club)
 			{
-				$lis=Array();
-				foreach ($arr['skills'] as $skillid)
-					if (sklearn_basecheck($skillid))
-						if ($callback_funcname('is_learnable',$skillid))
-							array_push($lis,$skillid);
-				
-				$sz=count($lis);
-				if ($sz%4==0) $lz=4; else if ($sz%3==0) $lz=3; else if ($sz%4>=$sz%3) $lz=4; else $lz=3;
-				$rz=floor($sz/$lz); $rz=(int)$rz; if ($sz%$lz!=0) $rz++;
-				for ($i=0; $i<$rz; $i++)
-				{
-					echo '<tr>';
-					if ($i==0) echo '<td class="b1" rowspan='.$rz.'>'.$clubinfo[$nowclub].'</td>';
-					$cz=$lz; if ($i==$rz-1 && $sz%$lz!=0) $cz=$sz%$lz;
-					for ($j=0; $j<$cz; $j++)
-					{
-						$skillid=$lis[$i*$lz+$j];
-						echo '<td class="b3">'.$clubskillname[$skillid].'</td>';
-						if (\skillbase\skill_query($skillid))
-						{
-							if ($is_show_cost) echo '<td class="b3"><span class="grey b">∞</span></td>';
-							echo '<td class="b3"><span class="grey b">已经获得</span></td>';
-						}
-						else
-						{
-							if ($is_show_cost)
-							{
-								$cost=$callback_funcname('query_cost',$skillid);
-								if ($cost>0)
-									echo '<td class="b3"><span class="lime b">'.$cost.'</span></td>';
-								else  echo '<td class="b3"><span class="grey b">-</span></td>';
-							}
-							echo '<td class="b3"><span style="width:40px;" onmouseover="$(\'skl_util_'.$caller_id.'_skilllearn_'.$skillid.'\').style.display=\'block\'; $(\'skl_util_'.$caller_id.'_skilllearn_'.$skillid.'\').style.top=Number(jQuery(this).offset().top-jQuery(window).scrollTop()+10).toString()+\'px\'; $(\'skl_util_'.$caller_id.'_skilllearn_'.$skillid.'\').style.left=Number(jQuery(this).offset().left-jQuery(window).scrollLeft()+40'.($j<3?'':'-460').').toString()+\'px\'; " onmouseout="$(\'skl_util_'.$caller_id.'_skilllearn_'.$skillid.'\').style.display=\'none\';"><input type="button" style="width:38px;" value="查看"></span>';
-							if ($callback_funcname('now_learnable',$skillid))
-								echo '<span style="width:40px;"><input type="button" style="width:38px" onclick="$(\'mode\').value=\'special\';$(\'command\').value=\'skill'.$caller_id.'_special\';$(\'subcmd\').value=\'upgrade\';$(\'skillpara1\').value=\''.$skillid.'\';postCmd(\'gamecmd\',\'command.php\');this.disabled=true;" value="学习"></span>';
-							else  echo '<span style="width:40px;"><input type="button" style="width:38px" disabled="true" value="学习"></span>';
-							array_push($___TEMP_tlis,$skillid);
-							echo '</td>';
+				foreach ($arr['skills'] as $skillid){
+					if (sklearn_basecheck($skillid)){
+						if ($callback_funcname('is_learnable',$skillid)){
+							$cost = $callback_funcname('query_cost',$skillid);
+							$now_learnable = $callback_funcname('now_learnable',$skillid);
+							if(!isset($lis[$nowclub])) $lis[$nowclub] = array();
+							array_push($lis[$nowclub],array($skillid, $cost, $now_learnable));
+							if(!\skillbase\skill_query($skillid)) array_push($___TEMP_tlis,$skillid);
 						}
 					}
-					for ($j=$cz; $j<4; $j++) 
-						if ($is_show_cost) 
-							echo '<td></td><td></td><td></td>';
-						else  echo '<td></td><td></td>';
-					echo '</tr>';
 				}
 			}
-		echo '</table>';
-		
+		include template(MOD_SKLEARN_UTIL_SKILLLEARN_TABLE);
 		$___TEMP_str='';
 		foreach ($___TEMP_tlis as $___TEMP_now_skillid)
 		{
@@ -96,13 +51,6 @@ namespace sklearn_util
 			include template(constant('MOD_SKILL'.$___TEMP_now_skillid.'_DESC'));
 			$str=ob_get_contents();
 			ob_end_clean();
-			
-//			$i=strpos($str,'_____TEMP_SKLEARN_START_FLAG_____')+strlen('_____TEMP_SKLEARN_START_FLAG_____');
-//			$j=strpos($str,'_____TEMP_SKLEARN_END_FLAG_____');
-//			$str = substr($str,$i,$j-$i);//取得desc.htm中间的介绍部分
-//			
-//			$i=strpos($str,'<td class="skilldesc_right b3">');
-//			if(false!==$i) $str = substr($str,0,$i);//去掉后边的操作单元格
 			$showcont = parse_skilllearn_desc($str);
 			unset($i); unset($j); unset($str);
 			$___TEMP_IN_SKLEARN_FLAG=0;
