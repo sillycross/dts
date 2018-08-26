cc9c0c55d101cefcaaf1ce98355291f09c6393a8
winddramon <ranshiin@qq.com>
2018-08-26 22:14:30 +0800

    1.13.0 indev 21

--- updatelist.php
+++ updatelist.php
@@ -3,8 +3,19 @@
 define('CURSCRIPT', 'updatelist');
 require './include/common.inc.php';
 
-
-include template('updatelist');
+if(empty($y)) include template('updatelist_container');
+elseif($y) {
+	$gamedata = array();
+	if(in_array($y, array('2017', '2016'))) {
+		ob_start();
+		include template('updatelist_'.$y);
+		$gamedata['innerHTML']['cont'.$y] = ob_get_contents();
+		ob_clean();
+		ob_end_flush();
+		if(isset($error)) $gamedata['innerHTML']['error'] = $error;
+		$jgamedata = gencode($gamedata);
+		echo $jgamedata;
+	}
+}
 
 ?>
-
