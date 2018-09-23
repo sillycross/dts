f6b1986b8ccf273c67a5eff2fdfaaa6b61ca9f7a
winddramon <ranshiin@qq.com>
2018-09-23 10:01:29 +0800

    1.13.0 indev 30

--- include/modules/base/itemmain/module.inc.php
+++ include/modules/base/itemmain/module.inc.php
@@ -7,7 +7,7 @@ namespace itemmain
 	$___MODULE_dependency_optional = 'explore npc';
 	$___MODULE_conflict = '';
 	$___MODULE_codelist = 'main.php itemuse.php itemcmd.php config/itemmain.config.php';
-	$___MODULE_templatelist = 'itemmerge itemmove itemdrop itemmerge0 itemdrop0 itemfind itmcommand profile_equipments profile_backpack';
+	$___MODULE_templatelist = 'itemmerge itemmove itemdrop itemmerge0 itemdrop0 itemfind itmcommand profile_equipments profile_equipments_short profile_backpack profile_backpack_short';
 	////////// MODULE HEADER END /////////////////
 	require __INIT_MODULE__(__NAMESPACE__,__DIR__);
 }
