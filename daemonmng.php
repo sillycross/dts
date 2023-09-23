ed5c804eb4c3020d2833a278874c6f85ac4f4ee7
winddramon <ranshiin@qq.com>
2023-09-23 23:54:34 +0800

    adjust daemonmng

--- daemonmng.php
+++ daemonmng.php
@@ -128,8 +128,9 @@ if ($handle=opendir(GAME_ROOT.'./gamedata/tmp/server'))
 			else echo "&nbsp;[根]&nbsp;进程 {$i}: 端口 <font color=\"blue\">{$key}</font> 状态 ";
 			if(file_exists($keyadrs.'/busy')) echo '<font color="yellow">正常&忙碌</font>';
 			else echo '<font color="green">正常</font>';
-			
-			echo '&nbsp;分均请求数：'.ceil((int)file_get_contents($keyadrs.'/worknum') / (max(1, $now - filemtime($keyadrs.'/start_time')) / 60) * 1000) / 1000;
+			list($worknum, $memorysize) = explode(',', file_get_contents($keyadrs.'/worknum'));
+			echo '&nbsp;分均请求数：'.ceil((int)$worknum / (max(1, $now - filemtime($keyadrs.'/start_time')) / 60) * 1000) / 1000;
+			echo '&nbsp;内存占用：'.ceil((int)$memorysize/1024/1024).'MB';
 		}
 		else
 		{
