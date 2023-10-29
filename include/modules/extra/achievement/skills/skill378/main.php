f62b20c06e5a84e01b41f4f3985698bbef0b8493
winddramon <ranshiin@qq.com>
2023-10-30 01:19:58 +0800

    add skill378(ach); rename card.css

---
+++ include/modules/extra/achievement/skills/skill378/main.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace skill378
+{
+	//各级要完成的成就名，如果不存在则取低的
+	$ach378_name = array(
+		1=>'<:secret:>H4sIAAAAAAAAClMKycgsVkhJrFQAUun5mXnpCiX5CkmpCgWpRWmpySV6SgCFucbKIgAAAA==',
+	);
+	
+	//各级显示的要求，如果不存在则取低的
+	$ach378_desc= array(
+		1=>'<:secret:>H4sIAAAAAAAAClWNQQ7AIAgE/+ILUBH0L1xqKz/g/109NOmFTDaTIVmwq1toK91iSAeL1suilUrYpTO4SgYXVgt3urEsFdzsc/sMkxetXRP6Nxsfn+n5OqeJJwPLnJJeuy26d4YAAAA=',
+	);
+	
+	$ach378_proc_words = '当前纪录';
+	
+	$ach378_unit = '次';
+	
+	//各级阈值，注意是达到这个阈值则升到下一级
+	$ach378_threshold = array(
+		1 => 1,
+		999 => NULL
+	);
+	
+	//各级给的切糕奖励
+	$ach378_qiegao_prize = array(
+		1 => 300,
+	);
+	
+	//各级给的卡片奖励
+	$ach378_card_prize = array(
+	);
+	
+	function init() 
+	{
+		define('MOD_SKILL378_INFO','achievement;secret;');
+		define('MOD_SKILL378_ACHIEVEMENT_ID','78');
+	}
+	
+	function acquire378(&$pa)
+	{
+		if (eval(__MAGIC__)) return $___RET_VALUE;
+	}
+	
+	function lost378(&$pa)
+	{
+		if (eval(__MAGIC__)) return $___RET_VALUE;
+	}
+	
+	function player_kill_enemy(&$pa,&$pd,$active){
+		if (eval(__MAGIC__)) return $___RET_VALUE;
+		$chprocess($pa, $pd, $active);		
+		//分两次判定，把需要import的条件放第二层
+		if(!empty($pa['card']) && \skillbase\skill_query(378,$pa) && !$pd['type'] && $pa['cardname']==$pd['cardname'])
+		{
+			eval(import_module('cardbase'));
+			if($cards[$pa['card']]['name'] != $pa['cardname']) {
+				$x=(int)\skillbase\skill_getvalue(378,'cnt',$pa);
+				$x+=1;
+				\skillbase\skill_setvalue(378,'cnt',$x,$pa);
+			}
+		}
+	}	
+	
+	function ach_finalize_process(&$pa, $data, $achid)
+	{
+		if (eval(__MAGIC__)) return $___RET_VALUE;
+		$ret = $chprocess($pa, $data, $achid);
+		if($achid == 378 && (int)\skillbase\skill_getvalue(378,'cnt',$pa)){
+			$ret += 1;
+		}
+		return $ret;
+	}
+}
+
+?>
