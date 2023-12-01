fd073dfd45ac59d9abfba8a1ca3243a3dfb020e0
satsuki8 <2893866331@qq.com>
2023-12-01 18:51:36 +0800

    add card 369

---
+++ include/modules/extra/card/skills/skill708/module.inc.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace skill708
+{
+	////////// MODULE HEADER START ///////////////
+	$___MODULE_dependency = 'skillbase clubbase sys player itemmix';
+	$___MODULE_dependency_optional = '';
+	$___MODULE_conflict = '';
+	$___MODULE_codelist = 'main.php';
+	$___MODULE_templatelist = 'desc';
+	////////// MODULE HEADER END /////////////////
+	require __INIT_MODULE__(__NAMESPACE__,__DIR__);
+}
+
+?>
