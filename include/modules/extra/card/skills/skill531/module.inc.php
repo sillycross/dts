770d0e6b6c5a10e66347e0772ae87d578dc8b1c5
satsuki8 <2893866331@qq.com>
2023-12-04 19:59:01 +0800

    skill531 update

--- include/modules/extra/card/skills/skill531/module.inc.php
+++ include/modules/extra/card/skills/skill531/module.inc.php
@@ -3,7 +3,7 @@
 namespace skill531
 {
 	////////// MODULE HEADER START ///////////////
-	$___MODULE_dependency = 'skillbase clubbase sys player logger attack battle';
+	$___MODULE_dependency = 'skillbase clubbase sys player logger attack battle itemmain itemmix';
 	$___MODULE_dependency_optional = '';
 	$___MODULE_conflict = '';
 	$___MODULE_codelist = 'main.php';
