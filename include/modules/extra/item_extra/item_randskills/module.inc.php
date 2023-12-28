c7d673b7565e3eedfb97e2769a8c72a2928d70f2
satsuki8 <2893866331@qq.com>
2023-12-28 23:41:17 +0800

    add item_randskills

---
+++ include/modules/extra/item_extra/item_randskills/module.inc.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace item_randskills
+{
+	////////// MODULE HEADER START ///////////////
+	$___MODULE_dependency = 'sys player skillbase itemmain attrbase logger input';
+	$___MODULE_dependency_optional = '';
+	$___MODULE_conflict = '';
+	$___MODULE_codelist = 'main.php';
+	$___MODULE_templatelist = 'use_skcore';
+	////////// MODULE HEADER END /////////////////
+	require __INIT_MODULE__(__NAMESPACE__,__DIR__);
+}
+
+?>
