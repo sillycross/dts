9ac6ff2278e61cba771ea7d1c94afd53ab5fbdd4
winddramon <ranshiin@qq.com>
2024-01-05 11:21:37 +0800

    修复了玩家最新一局游戏的记录错误

--- include/valid.func.php
+++ include/valid.func.php
@@ -121,7 +121,7 @@ function enter_battlefield($xuser,$xpass,$xgender,$xicon,$card=0,$ip='')
 	$db->array_insert("{$tablepre}players", $eb_pdata);
 	
 	//更新用户账户最后局数
-	$updatearr['lastgame'] = $room_prefix.$gamenum;
+	$updatearr['lastgame'] = !empty($room_prefix) ? substr($room_prefix,0,1) . $gamenum : $gamenum;
 	
 	//如果卡片合法，记录改变的卡
 	$upd_card = !empty($o_card) ? $o_card : $card;
