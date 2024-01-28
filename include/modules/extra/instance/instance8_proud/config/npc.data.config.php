b5ceedf21d97a8479d68c620bbe2ba129ecec7aa
satsuki8 <2893866331@qq.com>
2024-01-28 19:28:54 +0800

    update

--- include/modules/extra/instance/instance8_proud/config/npc.data.config.php
+++ include/modules/extra/instance/instance8_proud/config/npc.data.config.php
@@ -182,11 +182,14 @@ namespace instance8
 					'itme1' => 280,
 					'itms1' => 100,
 					'itmsk1' => 'ikj',
-					'itm2' => '汇灵戒·吞星',
-					'itmk2' => 'DAS',
-					'itme2' => 70,
-					'itms2' => 20,
-					'itmsk2' => '^st1^vol4',
+					
+					'ext_armor' => array(//需要外甲模块才能生效
+						'arb' => '暗红色的外套',
+						'arbk' => 'DBS',
+						'arbe' => 120,
+						'arbs' => 30,
+						'arbsk' => '^st1^vol4',
+					),
 					'description' => '全息幻象 幻影斗将神 S.A.S，本名叫做『Salina』，东之国著名角色扮演游戏《重生之地》的女主角之一，使用符合设定的巨大镰刀进行攻击。掉落优秀的斩系武器（可变为DN刀）。',
 				),
 				2 => array
