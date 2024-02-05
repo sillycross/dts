3badc7a9876538ec2288cc37dc638d726855da7f
satsuki8 <2893866331@qq.com>
2024-02-05 19:18:21 +0800

    add skill733

---
+++ include/modules/extra/card/skills/skill733/module.inc.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace skill733
+{
+	////////// MODULE HEADER START ///////////////
+	$___MODULE_dependency = 'sys player skillbase clubbase searchmemory skill1006';
+	$___MODULE_dependency_optional = '';
+	$___MODULE_conflict = '';
+	$___MODULE_codelist = 'main.php';
+	$___MODULE_templatelist = 'profilecmd castsk733';
+	////////// MODULE HEADER END /////////////////
+	require __INIT_MODULE__(__NAMESPACE__,__DIR__);
+}
+
+?>
